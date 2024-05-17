db7b01b747daf68402bf78d8ea836e8f554cc13a
Rfccoelho <rfcrafael55@gmail.com>
2024-05-16 21:36:53 -0300

    Conta-Banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,40 @@
+
+import java.util.Locale;
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+
+        
+Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("===========================");
+        System.out.println("Digite numero da conta:");
+        int conta = scanner .nextInt();
+
+        System.out.println("Digite a agencia: ");
+        String agencia = scanner .next();
+
+        System.out.println("Nome do cliente: ");
+        String cliente = scanner .next();
+
+        System.out.println("Digite o valor de deposito: ");
+        Float deposito = scanner .nextFloat();
+        System.out.println("________________________________________________________________");
+        
+        System.out.println("Olá, " +cliente+ " Obrigado por criar uma conta em nosso banco.");
+        System.out.println("*******************************");
+        System.out.println("Sua Agencia: " +agencia);
+        System.out.println("*******************************");
+        System.out.println("Conta: " +conta );
+        System.out.println("*******************************");
+        System.out.println("Seu Saldo: " +deposito); 
+        System.out.println("*******************************");
+        System.out.println(" Voce já pode fazer saque e deposito na sua conta.");
+        System.out.println("*******************************");
+
+        System.out.println("*****NÃO COMPARTILHE OS DADOS Da SUA CONTA******");
+        System.out.println("__________________________________________________________________");
     }
-}
+
+   
+    }
